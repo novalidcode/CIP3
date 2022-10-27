d4986d2aa59df6630a033eb56fa2902e1861bf05
novalidcode <tobias.strom@fh-bielefeld.de>
2022-10-27 15:47:49 +0200

    TS V1.01

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# INF3_PRAK
-Source code for practicals WS2019/2020
+# CIP3
+Source code for CIP3 WS2022/2023
+
+
+## Brief Explanation
+
+Schach => *[chess](https://en.wikipedia.org/wiki/Chess)*
+
+Rochade => *[castling](https://en.wikipedia.org/wiki/castling)*
+
+En passant => *[en passant](https://en.wikipedia.org/wiki/En_passant)*
+
+
+
+###Help Links
+
+format .md files [Markdown](https://de.wikipedia.org/wiki/Markdown)
--- chess.cpp
+++ chess.cpp
@@ -1,9 +1,44 @@
 #include "chess.hpp"
 
+/*
+ * 	Schachspiel::checkArea almost finished
+ * 		castling & attackedField
+ *
+ * 	Schachspiel::checkPawn almost finished
+ * 		en passant
+ * 		upgradePawn => Queen, Roook, Knight or Bishop when reached other side
+ *
+ *	Schachspiel::isValid not implemented here
+ *
+ * 	methode needed bool checkProtectKing(int sx, int sy){ } //Checks if the King is protected by a Figure, needs to be checked for all
+ * 	maybe just put it just inside the isvalid methode?
+ *
+ *
+ * 	English			German
+ * 	-------------------------------------
+ * 	Chess			Schach
+ * 	check			Schach
+ * 	check mate		Schachmatt
+ *
+ * 	Special moves
+ *
+ * 	castling		Rochade		https://en.wikipedia.org/wiki/castling
+ *	En passant		en passant	https://en.wikipedia.org/wiki/En_passant
+ *
+ * 	Chessboard		Schachbrett
+ * 	Pawn			Bauer
+ * 	Rook			Turm
+ * 	Knight			Springer
+ * 	Bishop			Läufer
+ * 	Queen			Königin
+ * 	King			König
+ *
+ */
 
+
+
+//Finished
 void Schachbrett::print(){
-
-
 
   for(int i=7;i>-1; i--){
 	cout <<i+1<<"  ";
@@ -17,27 +52,105 @@ void Schachbrett::print(){
       cout <<endl;
   }
 
+};
+
+//Finished
+bool Schachspiel::checkDiagonal(int sx, int sy, int zx, int zy){
+  //First check start and end position color, if true then return false, this add as methode? //Catches also if the same place is tried to use (1,1) (1,1) => false
+            if(isEmptySpace(board[sx][sy]) || (isW(board[sx][sy])&&isW(board[zx][zy])) || (isB(board[sx][sy])&&isB(board[zx][zy]))) //Check is there a Figure to move // check same color
+              return false;
 
 
-}
-bool checkDiagonal(int sx, int sy, int zx, int zy){
-  //(1,1) -> (5,5)
+  //Check if Diagonal
   if(abs(sx-zx)==abs(sy-zy)){
+      //Check if something betweeen
+      int g= sx<zx ? 1: -1;
+      int ge = sy<zy ? 1: -1;
+      for(int i=1; i<abs(sx-zx)-1; i++)		//-1 because of the first check and +1 because not the same field
+	if(!isEmptySpace(board[sx+(g*i)][sy+(ge*i)]))
+	  return false;
 
   }
   return false;
-}
-   bool checkNESW(int sx, int sy, int zx, int zy){
-     return sx == zx ? true: sy==zy ? true: false ;
+};
+   // Finished
+   bool Schachspiel::checkNESW(int sx, int sy, int zx, int zy){
+     //First check start and end position color, if true then return false, this add as methode? //Catches also if the same place is tried to use (1,1) (1,1) => false
+          if(isEmptySpace(board[sx][sy]) || (isW(board[sx][sy])&&isW(board[zx][zy])) || (isB(board[sx][sy])&&isB(board[zx][zy]))) //Check is there a Figure to move // check same color
+            return false;
+
+
+          int g=1;	//pos. or neg. way
+          if(sx == zx){
+              g= sy<zy? 1:-1;
+              //check emptyness
+              for(int i=1; i<abs(sy-zy)-1; i++)		//-1 because of the first check and +1 because not the same field
+        	if(!isEmptySpace(board[sx][sy+(g*i)]))
+        	  return false;
+              return true;
+            }
+          else if(sy==zy){
+              g= sx<zx? 1:-1;
+              //check emptyness
+	      for(int i=1; i<abs(sx-zx)-1; i++)		//-1 because of the first check and +1 because not the same field
+		  if(!isEmptySpace(board[sx+(g*i)][sy]))
+		    return false;
+	      return true;
+          }
+          else{
+              return false;
+          }
 
 
 
-   }
-   bool checkArea(int sx, int sy, int zx, int zy){
-       return (board[zx][zy]!='#' || board[zx][zy]!=' ') ;
 
-   }
-   bool checkKnight(int sx, int sy, int zx, int zy){
+
+   };
+
+   //Not finished yet
+   bool Schachspiel::checkArea(int sx, int sy, int zx, int zy){
+     //First check start and end position color, if true then return false, this add as methode? //Catches also if the same place is tried to use (1,1) (1,1) => false
+     if(isEmptySpace(board[sx][sy]) || (isW(board[sx][sy])&&isW(board[zx][zy])) || (isB(board[sx][sy])&&isB(board[zx][zy]))) //Check is there a Figure to move // check same color
+       return false;
+     //King issue that he can't move on a protected/attacked field // maybe write a methode to figure it out if there is a protection / attack,
+     //Could also be used for the rouchade
+
+     //Still Missing
+       //bool checkPosAttacked?(zx,zy){ knight?, Pawn near?  Rook? Bishop? (Queen)? } //King && King 1 empty Field betweeen
+       //castling methode? member Array bool moved_[6] of 6 bool values for , R K R, R K R
+
+     if (sy == zy){
+	if(sx-1==zx || sx+1==zx){
+	    return true;
+	}
+	else{
+	    return false;
+	}
+     }
+     else if (sy+1 == zy){
+	if(sx-1==zx || sx==zx ||sx+1==zx ){
+	   return true;
+	}
+	else{
+	   return false;
+	}
+     }
+     else if (sy-1 == zy){
+	if(sx-1==zx || sx==zx ||sx+1==zx ){
+	    return true;
+	}
+	else{
+	   return false;
+	}
+     }
+     else{
+	 return false;
+     }
+
+   };
+
+   //Finished
+   bool Schachspiel::checkKnight(int sx, int sy, int zx, int zy){
      int g[] = {2, -2};
      int ge[] = {1, -1};
 
@@ -51,15 +164,45 @@ bool checkDiagonal(int sx, int sy, int zx, int zy){
        		return true;
 
      return false;
-   }
-   bool checkPawn(int sx, int sy, int zx, int zy){
-     //
-     if(sy==6 || sy==1 )
-	 ;
-     return;
-   }
+   };
+   //almost finished
+   bool Schachspiel::checkPawn(int sx, int sy, int zx, int zy){
+     //First check start and end position color, if true then return false, this add as methode?
+
+     //en passant a bit complicated perhaps
+
+     if(isEmptySpace(board[sx][sy]) || (isW(board[sx][sy])&&isW(board[zx][zy])) || (isB(board[sx][sy])&&isB(board[zx][zy]))) //Check is there a Figure to move // check same color
+       return false;
+
+     bool notmoved=(sy==6 || sy==1 );		//Pawn at Startpoint
+
+     //Find direction
+     int ge = isW(board[sx][sy])? abs(sy-zy) :-abs(sy-zy);
+
+     if(abs(ge)>2 || (abs(ge)>1 && sx!=zx) )
+       return false; //Out of Range | wrong move sidewards
+
+     //Vertical move
+     if(sx == zx){
+	 //check Emptyness
+	 for(int i=0; i<abs(ge); i++){
+	    if(!isEmptySpace(board[sx][sy+((i+1)*(ge/ge))]))
+	      return false;
+	 }
+	 return true;
+     }
+
+     if(sx!=zx){ //Check if hit other color --- --- en passant special implement later, need to know the last move of the other player
+	 if(!isEmptySpace(board[zx][zy]))
+	   return true;
+	 //else if(enpassant)
+	 else{
+	     return false;
+	 }
+     }
+
+     return true;
+   };
 
 
-   bool isValid(int sx, int sy, int zx, int zy){
-     return;
-   }
+
